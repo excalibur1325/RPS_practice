8fa18ead1fa8c79b993be298755595888798ad55
Darren Cruz <excalibur.dc1325@gmail.com>
2025-09-23 19:49:13 -0400

    Procaticing more python

---
+++ range.py
@@ -0,0 +1,10 @@
+# i - iterator / counter variable
+# range() - class that takes in numbers as arguments
+# arguments - values passed into the class constructor 
+
+
+#  for i in range(10):
+#    print(i)
+
+for i in range(1, 19, 4):
+    print(i)
