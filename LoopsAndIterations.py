204cc43a0594c4f02916cd775f85213d011b33ee
Darren Cruz <excalibur.dc1325@gmail.com>
2025-09-26 19:22:46 -0400

    practicing with loops

---
+++ LoopsAndIterations.py
@@ -0,0 +1,25 @@
+
+#nums = [1, 2, 3, 4, 5]
+
+# for num in nums:
+    # conditional
+   # if num == 3:
+    #    print('Found!')
+        # break statement to break out of the "for" loop
+        #break
+         # 'continue' statement will skip to the next iteration 
+    #    continue
+    #print(num)
+
+# loop within a loop (nested loops)
+#for num in nums:
+ #   for letter in 'abc':
+  #      print(num, letter)
+
+# "while" loops will continue until a conditional is met or when a 'break' happens
+x = 0
+#condition
+while x < 7:
+    print(x)
+    #increment(s)
+    x += 1
